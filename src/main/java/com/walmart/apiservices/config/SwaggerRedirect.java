9ac6054fbb3796f2c4ce1e9d4f5f2b8cd1d7be99
sokar311 <ceincomp@gmail.com>
2020-11-15 03:58:19 -0300

    Deploy

--- src/main/java/com/walmart/apiservices/config/SwaggerRedirect.java
+++ src/main/java/com/walmart/apiservices/config/SwaggerRedirect.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SwaggerRedirect implements WebMvcConfigurer {
 
+	
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/swagger-ui/index.html");
